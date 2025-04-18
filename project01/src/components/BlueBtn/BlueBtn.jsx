c5b91399be987bd659b1c1aabe51ceff6e2558e9
Neskajy <besonovskiyyy@gmail.com>
2025-04-18 17:30:35 +0300

    delete 01

--- project01/src/components/BlueBtn/BlueBtn.jsx
+++
@@ -1,6 +0,0 @@
-import s from './BlueBtn.module.css'
-export default function BlueBtn({text='Кнопка по умолчанию'}){
-    return(
-        <button className={s.btn}>{text}</button>
-    )
-}
--- project01/src/pages/CatalogPage/Search/Search.jsx
+++
@@ -1,9 +0,0 @@
-export default function Search({handleChange}){
-    return(
-        <input onChange={handleChange} tipe='text'
-        placeholder="Поиск..." stile=
-        {{border: '1px solid #e5e5e5',
-        padding:'40px', marginBottom:
-        "40px", marginTop:'40px'}}/>
-    )
-}
